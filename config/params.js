ccc1bcd2552bccb4a41d6678514482c4b6efc5f6
Jatin <jatin@furlenco.com>
2016-11-20 18:02:15 +0530

    prod fb

--- config/params.js
+++ config/params.js
@@ -13,5 +13,10 @@ module.exports = {
     "port": process.env.PORT,
     "db_url": process.env.DB_URL,
     "secret": "thisisasecret"
+  },
+  "fb":{
+    "app_id": "1761542434088170",
+    "app_secret": "c1e3f7c9751b94a639ee77b85cec1430",
+    "callbackUrl": "http://localhost:5000/auth/facebook/callback"
   }
 }
